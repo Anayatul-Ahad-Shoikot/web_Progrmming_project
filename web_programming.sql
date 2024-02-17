bb3940a44b78beb12f2082ecc21745bb12982da6
Anayatul Ahad Shoikot <aashoikot002@gmail.com>
2024-02-18 01:00:28 +0600

    r

--- web_programming.sql
+++ web_programming.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2024 at 06:48 PM
+-- Generation Time: Feb 17, 2024 at 07:59 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -43,7 +43,8 @@ CREATE TABLE `accounts` (
 INSERT INTO `accounts` (`ac_id`, `username`, `useremail`, `userpassword`, `priority`, `img`) VALUES
 (4, 'AHAD', 'a@gmail.com', '$2y$10$NAyLysF7W3vke9rNO0JcTerVWmswzrhkhdL5aE8egs0.mdvKiLrwi', 1, 'img/416226397_799008138910419_2788070358869374265_n.jpg'),
 (6, 'admin2', 'admin2@gmail.com', '$2y$10$1QZurPwfplIFUkArBaG1H.L991kCMiLaRHtjXNlHm9VnxcbO3v3Xq', 1, 'img/011212107.jpg'),
-(12, 'admin9', 'admin9@gmail.com', '$2y$10$evLQ7Ch4Iz48TSdOMQtAqOxLcOP/FOzO5O.Bqzvmu5zJ8v/VY3zmS', 1, '');
+(12, 'admin9', 'admin9@gmail.com', '$2y$10$evLQ7Ch4Iz48TSdOMQtAqOxLcOP/FOzO5O.Bqzvmu5zJ8v/VY3zmS', 1, ''),
+(14, 'admin1000', 'admin10@gmail.com', '$2y$10$Wnsxc2bMb63lQYstSKYjpuG9iw5bmOb828yvtQ3I4e/jO042Kp9Ou', 0, '');
 
 -- --------------------------------------------------------
 
@@ -53,43 +54,52 @@ INSERT INTO `accounts` (`ac_id`, `username`, `useremail`, `userpassword`, `prior
 
 CREATE TABLE `history` (
   `his_id` int(3) NOT NULL,
-  `key_word` varchar(20) NOT NULL,
-  `content` varchar(255) NOT NULL
+  `content_key` varchar(50) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `user` varchar(100) NOT NULL,
+  `content_activity` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `history`
 --
 
-INSERT INTO `history` (`his_id`, `key_word`, `content`) VALUES
-(1, 'note created', 'A note is created for ahad at 2024-02-15 20:04.'),
-(2, 'note created', 'A note is created for KMAE at 2024/02/16  04:29.'),
-(3, 'Note Deleted', 'You deleted a note note id (4) at 2024/02/16  04:44.'),
-(4, 'Note Deleted', 'You deleted a note note id (3) at 2024/02/16  10:18.'),
-(5, 'note created', 'A note is created for AAS at 2024/02/16  10:20.'),
-(6, 'Note Deleted', 'You deleted a note note id (5) at 2024/02/16  10:20.'),
-(7, 'note created', 'A note is created for aas at 2024/02/16  10:20.'),
-(8, 'note created', 'A note is created for asdwd at 2024/02/16  10:21.'),
-(9, 'note created', 'A note is created for awds at 2024/02/16  10:22.'),
-(10, 'Note Deleted', 'You deleted a note note id (6) at 2024/02/16  10:22.'),
-(11, 'Note Deleted', 'You deleted a note note id (7) at 2024/02/16  10:22.'),
-(12, 'Note Deleted', 'You deleted a note note id (8) at 2024/02/16  10:22.'),
-(13, 'note created', 'A note is created for awdsa at 2024/02/16  10:24.'),
-(14, 'note created', 'A note is created for awds at 2024/02/16  10:24.'),
-(15, 'Note Deleted', 'You deleted a note note id (9) at 2024/02/16  10:27.'),
-(16, 'Note Deleted', 'You deleted a note note id (10) at 2024/02/16  10:27.'),
-(17, 'Notes Created', 'Note created for test by (2024/02/16  11:07).'),
-(18, 'Note Deleted', 'admin deleted a note(Note id = 11) at 2024/02/16  11:08.'),
-(19, 'Notes Created', 'Note created for asdwad by admin(2024/02/16  11:09).'),
-(20, 'Note Deleted', 'admin deleted a note(Note id = 12) at 2024/02/16  11:23.'),
-(21, 'Notes Created', 'Note created for efe by admin(2024/02/16  11:30).'),
-(22, 'Note Deleted', 'admin deleted a note(Note id = 13) at 2024/02/16  11:30.'),
-(23, 'Username changed', 'Username changed from admin to admin by admin at 2024/02/16  11:44).'),
-(24, 'Image Update', 'Image updated byadmin at 2024/02/16  11:57).'),
-(25, 'Username changed', 'Username changed from admin to AHAD by admin at (2024/02/16  11:59).'),
-(26, 'Useremail changed', 'Email changed from  to ahad@gmail.com by -AHAD at (2024/02/16  12:01).'),
-(27, 'Useremail changed', 'Email changed from  to a@gmail.com by -AHAD at (2024/02/16  12:02).'),
-(28, 'Note', 'Note created for Nahid Hossain by AHAD(2024/02/16  12:14).');
+INSERT INTO `history` (`his_id`, `content_key`, `content`, `user`, `content_activity`) VALUES
+(1, 'note created', 'A note is created for ahad at 2024-02-15 20:04.', '', ''),
+(2, 'note created', 'A note is created for KMAE at 2024/02/16  04:29.', '', ''),
+(3, 'Note Deleted', 'You deleted a note note id (4) at 2024/02/16  04:44.', '', ''),
+(4, 'Note Deleted', 'You deleted a note note id (3) at 2024/02/16  10:18.', '', ''),
+(5, 'note created', 'A note is created for AAS at 2024/02/16  10:20.', '', ''),
+(6, 'Note Deleted', 'You deleted a note note id (5) at 2024/02/16  10:20.', '', ''),
+(7, 'note created', 'A note is created for aas at 2024/02/16  10:20.', '', ''),
+(8, 'note created', 'A note is created for asdwd at 2024/02/16  10:21.', '', ''),
+(9, 'note created', 'A note is created for awds at 2024/02/16  10:22.', '', ''),
+(10, 'Note Deleted', 'You deleted a note note id (6) at 2024/02/16  10:22.', '', ''),
+(11, 'Note Deleted', 'You deleted a note note id (7) at 2024/02/16  10:22.', '', ''),
+(12, 'Note Deleted', 'You deleted a note note id (8) at 2024/02/16  10:22.', '', ''),
+(13, 'note created', 'A note is created for awdsa at 2024/02/16  10:24.', '', ''),
+(14, 'note created', 'A note is created for awds at 2024/02/16  10:24.', '', ''),
+(15, 'Note Deleted', 'You deleted a note note id (9) at 2024/02/16  10:27.', '', ''),
+(16, 'Note Deleted', 'You deleted a note note id (10) at 2024/02/16  10:27.', '', ''),
+(17, 'Notes Created', 'Note created for test by (2024/02/16  11:07).', '', ''),
+(18, 'Note Deleted', 'admin deleted a note(Note id = 11) at 2024/02/16  11:08.', '', ''),
+(19, 'Notes Created', 'Note created for asdwad by admin(2024/02/16  11:09).', '', ''),
+(20, 'Note Deleted', 'admin deleted a note(Note id = 12) at 2024/02/16  11:23.', '', ''),
+(21, 'Notes Created', 'Note created for efe by admin(2024/02/16  11:30).', '', ''),
+(22, 'Note Deleted', 'admin deleted a note(Note id = 13) at 2024/02/16  11:30.', '', ''),
+(23, 'Username changed', 'Username changed from admin to admin by admin at 2024/02/16  11:44).', '', ''),
+(24, 'Image Update', 'Image updated byadmin at 2024/02/16  11:57).', '', ''),
+(25, 'Username changed', 'Username changed from admin to AHAD by admin at (2024/02/16  11:59).', '', ''),
+(26, 'Useremail changed', 'Email changed from  to ahad@gmail.com by -AHAD at (2024/02/16  12:01).', '', ''),
+(27, 'Useremail changed', 'Email changed from  to a@gmail.com by -AHAD at (2024/02/16  12:02).', '', ''),
+(28, 'Note', 'Note created for Nahid Hossain by AHAD(2024/02/16  12:14).', '', ''),
+(29, 'Note', 'Note created for Tanjum by AHAD(2024/02/17  18:07).', 'AHAD', 'create'),
+(30, 'Note', 'AHAD deleted a note (Note id = 14) at \\t2024/02/17  18:07.', 'AHAD', 'delete'),
+(31, 'Note', 'Note created for SHOIKOT by admin10(2024/02/17  18:51).', 'admin10', 'create'),
+(32, 'Note', 'Deleted a note [Note id = 15] 2024/02/17  18:53 ---admin10', 'admin10', 'delete'),
+(33, 'username', 'Username changed from admin10 to admin1000 (2024/02/17  18:53) ---admin10', 'admin10', 'Change'),
+(34, 'Note', 'Deleted a note [Note id = 16] 2024/02/17  19:10 ---admin1000', 'admin1000', 'delete'),
+(35, 'Note', 'Note created for asdaw (2024/02/17  19:25) ---admin1000', 'admin1000', 'create');
 
 -- --------------------------------------------------------
 
@@ -109,18 +119,21 @@ CREATE TABLE `notes` (
 --
 
 INSERT INTO `notes` (`note_id`, `note_for`, `note_content`, `visibility`) VALUES
-(3, 'ahad', 'this is second test for me', 0),
+(3, 'ahad', 'this is second test for me', 1),
 (4, 'KMAE', '2nd test.', 0),
-(5, 'AAS', 'don\'t know how to implement', 0),
+(5, 'AAS', 'don\'t know how to implement', 1),
 (6, 'aas', 'asdweafasdf', 0),
 (7, 'asdwd', 'asdWd', 0),
-(8, 'awds', 'awd', 0),
-(9, 'awdsa', 'asdWdf', 0),
-(10, 'awds', 'adsadw', 0),
+(8, 'awds', 'awd', 1),
+(9, 'awdsa', 'asdWdf', 1),
+(10, 'awds', 'adsadw', 1),
 (11, 'test', 'test test test test test test test test test test test test.', 0),
-(12, 'asdwad', 'SdcrfugpSDOJF LASJ:KFHa', 0),
-(13, 'efe', 'ASDFqwd', 0),
-(14, 'Nahid Hossain', 'Web Programming - (2:00pm-4:30pm)', 1);
+(12, 'asdwad', 'SdcrfugpSDOJF LASJ:KFHa', 1),
+(13, 'efe', 'ASDFqwd', 1),
+(14, 'Nahid Hossain', 'Web Programming - (2:00pm-4:30pm)', 0),
+(15, 'Tanjum', 'Hi tanjum, how are you?', 1),
+(16, 'SHOIKOT', 'hi hello bye good bad', 0),
+(17, 'asdaw', 'SDWDad ', 1);
 
 --
 -- Indexes for dumped tables
@@ -155,19 +168,19 @@ ALTER TABLE `notes`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `ac_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `ac_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `his_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `his_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `note_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `note_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
