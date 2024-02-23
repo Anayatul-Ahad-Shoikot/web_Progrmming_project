4cab913db72d7fc7f07586a3da53b6a196de0d88
Anayatul-Ahad-Shoikot <aashoikot002@gmail.com>
2024-02-23 11:22:34 +0600

    ''

--- timeline/timeline.php
+++ timeline/timeline.php
@@ -169,7 +169,7 @@
                                 $result = $con->query($sql);
                                 if ($result->num_rows > 0) {
                                     while ($row = $result->fetch_assoc()) {
-                                        echo '<option value="' . $row['c_name'] . '" data-code="' . $row['c_code'] . '" data-type="' . $row['c_type'] . '" data-sec="' . $row['c_sec'] . '" data-time="' . $row['c_time'] . '" data-day="' . $row['c_day'] . '">' . $row['c_name'] . '</option>';
+                                        echo '<option value="' . $row['c_name'] . '" data-code="' . $row['c_code'] . '" data-type="' . $row['c_type'] . '" data-sec="' . $row['c_sec'] . '" data-time="' . $row['c_time'] . '" data-day="' . $row['c_day'] . '">' . $row['c_name'] . ' - lab - '.$row['c_sec'] .'</option>';
                                     }
                                 }
                                 $con->close();
